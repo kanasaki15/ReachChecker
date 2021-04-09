d269b2b298f3cc9ddd32f29a4dfef42a61237f96
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-04-10 03:23:09 +0900

    とても自分なりにはいいチェック

--- src/main/java/xyz/n7mn/dev/reachchecker/Command.java
+++ src/main/java/xyz/n7mn/dev/reachchecker/Command.java
@@ -10,14 +10,21 @@ public class Command implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (ReachChecker.map.containsKey(player.getUniqueId())) {
-                if (ReachChecker.map.get(player.getUniqueId()).equals("alert.true")) {
-                    ReachChecker.map.put(player.getUniqueId(), "alert.false");
-                    player.sendMessage("§e[ReachChecker] §rアラートを§cOFF§rにしました");
-                } else {
-                    ReachChecker.map.put(player.getUniqueId(), "alert.true");
-                    player.sendMessage("§e[ReachChecker] §rアラートを§aON§rにしました");
+                String alerts = ReachChecker.map.get(player.getUniqueId());
+
+                switch (alerts) {
+                    case "alert.true":
+                        ReachChecker.map.put(player.getUniqueId(), "alert.false");
+                        player.sendMessage("§e[ReachChecker] §rアラートを§cOFF§rにしました");
+                        break;
+                    case "alert.false":
+                        ReachChecker.map.put(player.getUniqueId(), "alert.true");
+                        player.sendMessage("§e[ReachChecker] §rアラートを§aON§rにしました");
+                        break;
+                    default:
+                        player.sendMessage("§c[ReachChecker] エラー...");
                 }
-            } else {
+            }else {
                 ReachChecker.map.put(player.getUniqueId(), "alert.true");
                 player.sendMessage("§e[ReachChecker] §rアラートを§aON§rにしました");
             }
