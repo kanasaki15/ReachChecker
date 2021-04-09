3389e0249c9dea8c7e9116c435900414a6dcfbb6
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-04-09 14:38:03 +0900

    Update EventListener.java

--- src/main/java/xyz/n7mn/dev/reachchecker/EventListener.java
+++ src/main/java/xyz/n7mn/dev/reachchecker/EventListener.java
@@ -20,44 +20,8 @@ class EventListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void d1(EntityDamageByEntityEvent e) {
+    public void d(EntityDamageByEntityEvent e) {
 
-        Entity damager = e.getDamager();
-        Entity entity = e.getEntity();
-
-        if (entity instanceof Player && damager instanceof Player) {
-            new Thread(() -> {
-                Player targetPlayer = (Player) entity;
-                Player fromPlayer = (Player) damager;
-                if (targetPlayer.getNoDamageTicks() == 20) {
-                    if (fromPlayer.getGameMode() != GameMode.CREATIVE) {
-                        double x = targetPlayer.getLocation().getX() - fromPlayer.getLocation().getX();
-                        x = x * x;
-                        double z = targetPlayer.getLocation().getZ() - fromPlayer.getLocation().getZ();
-                        z = z * z;
-                        double y = Math.abs(targetPlayer.getLocation().getY() - fromPlayer.getLocation().getY());
-                        double distance = Math.sqrt(x + z) - (y / 2.8);
-                        plugin.getLogger().info(fromPlayer.getName() + " ---> " + targetPlayer.getName() + " : " + distance);
-                        if (distance >= 3.5) {
-
-                            for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-
-                                if (player.isOp() || player.hasPermission("reachchecker.op")) {
-
-                                    player.sendMessage("" +
-                                            ChatColor.YELLOW + "[ReachChecker(A)] " + ChatColor.RESET + fromPlayer.getName() + " : " + distance
-                                    );
-                                }
-                            }
-                        }
-                    }
-                }
-            }).start();
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void d2(EntityDamageByEntityEvent e) {
         Entity damager = e.getDamager();
         Entity entity = e.getEntity();
 
@@ -66,23 +30,25 @@ class EventListener implements Listener {
                 Player targetPlayer = (Player) entity;
                 Player fromPlayer = (Player) damager;
                 if (fromPlayer.getGameMode() != GameMode.CREATIVE) {
-                    double distance = targetPlayer.getLocation().distance(fromPlayer.getLocation());
+                    double x = targetPlayer.getLocation().getX() - fromPlayer.getLocation().getX();
+                    x = x * x;
+                    double z = targetPlayer.getLocation().getZ() - fromPlayer.getLocation().getZ();
+                    z = z * z;
                     double y = Math.abs(targetPlayer.getLocation().getY() - fromPlayer.getLocation().getY());
-                    if (y <= 3.5) {
-                        plugin.getLogger().info(fromPlayer.getName() + " ---> " + targetPlayer.getName() + " : " + distance);
-                        if (distance >= 4.1) {
+                    double distance = Math.sqrt(x + z) - (y / 2.5); //1.12.2の場合は下げてもいいかも
+                    plugin.getLogger().info(fromPlayer.getName() + " ---> " + targetPlayer.getName() + " : " + distance);
+                    if (distance >= 3.5) {
 
-                            for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+                        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
 
-                                if (player.isOp() || player.hasPermission("reachchecker.op")) {
+                            if (player.isOp() || player.hasPermission("reachchecker.op")) {
 
-                                    player.sendMessage("" +
-                                            ChatColor.YELLOW + "[ReachChecker(B)] " + ChatColor.RESET + fromPlayer.getName() + " : " + distance
-                                    );
-                                }
+                                player.sendMessage("" +
+                                        ChatColor.YELLOW + "[ReachChecker] " + ChatColor.RESET + fromPlayer.getName() + " : " + distance
+                                );
                             }
                         }
-                    }else plugin.getLogger().info("[Canceled Check]" + fromPlayer.getName() + " ---> " + targetPlayer.getName() + " : " + distance + "(" + y + ")");
+                    }
                 }
             }).start();
         }
