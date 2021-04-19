15bb80225f67d185338779e58c62101de1d6861c
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-04-19 20:11:46 +0900

    ミス

--- src/main/java/xyz/n7mn/dev/reachchecker/ReachChecker.java
+++ src/main/java/xyz/n7mn/dev/reachchecker/ReachChecker.java
@@ -3,24 +3,26 @@ package xyz.n7mn.dev.reachchecker;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
-public final class ReachChecker extends JavaPlugin {
 
+public final class ReachChecker extends JavaPlugin {
     public static HashMap<UUID,String> map = new HashMap<>();
     public static HashMap<UUID,Integer> VLA = new HashMap<>();
     public static HashMap<UUID,Integer> VLB = new HashMap<>();
     public static HashMap<UUID,Double> LastReach = new HashMap<>();
     public static HashMap<UUID,Double> MaxReach = new HashMap<>();
+    public static HashMap<UUID,Integer> PreviewCPS = new HashMap<>();
+    public static HashMap<UUID,Boolean> ActionBar = new HashMap<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
         getServer().getPluginManager().registerEvents(new EventListener(this), this);
         getCommand("alerts").setExecutor(new AlertsCommand());
         getCommand("look-up").setExecutor(new LookUpCommand());
+        getCommand("cps").setExecutor(new GetCPSCommand(this));
+
     }
 
     @Override
