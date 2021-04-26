4e9748c08ca4b2bed238dc78ec2bf158d18f563e
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-04-26 20:36:10 +0900

    意味わからない処理をしていたのを改善

--- src/main/java/xyz/n7mn/dev/reachchecker/EventListener.java
+++ src/main/java/xyz/n7mn/dev/reachchecker/EventListener.java
@@ -47,14 +47,14 @@ class EventListener implements Listener {
                         double distance = Math.sqrt(x + z) - (y / 7.5); //1.8: 2.5//1.12.2: 7.5
                         data.setLastreach(distance);
                         plugin.getLogger().info(fromPlayer.getName() + " ---> " + targetPlayer.getName() + " : " + distance + " (A)");
+                        if (distance > data.getMaxreach()) {
+                            data.setMaxreach(distance);
+                        }
                         if (distance >= 4.0 && 12.0 >= distance) {
                             data.setVLA(data.getVLA() + 1);
                             for (Player player : Bukkit.getServer().getOnlinePlayers()) {
                                 if (ReachChecker.playerdataHashMap.get(player.getUniqueId()).isAlert()) {
                                     player.sendMessage("" + ChatColor.YELLOW + "[ReachChecker(A)] " + ChatColor.RESET + fromPlayer.getName() + " : " + distance + " §6§l(" + data.getVLA() + ")");
-                                }
-                                if (distance > data.getMaxreach()) {
-                                    data.setMaxreach(distance);
                                 }
                             }
                         }
@@ -63,14 +63,14 @@ class EventListener implements Listener {
                         double distance = fromPlayer.getLocation().distance(targetPlayer.getLocation());
                         data.setLastreach(distance);
                         plugin.getLogger().info(fromPlayer.getName() + " ---> " + targetPlayer.getName() + " : " + distance + " (B)");
+                        if (distance > data.getMaxreach()) {
+                            data.setMaxreach(distance);
+                        }
                         if (distance >= 4.0 && 12.0 >= distance) { //1.8: 2.5//1.12.2: 7.5
                             data.setVLB(data.getVLB() + 1);
                             for (Player player : Bukkit.getServer().getOnlinePlayers()) {
                                 if (ReachChecker.playerdataHashMap.get(player.getUniqueId()).isAlert()) {
                                     player.sendMessage("" + ChatColor.GOLD + "[ReachChecker(B)] " + ChatColor.RESET + fromPlayer.getName() + " : " + distance + " §6§l(" + data.getVLB() + ")");
-                                }
-                                if (distance > data.getMaxreach()) {
-                                    data.setMaxreach(distance);
                                 }
                             }
                         }
